0c10e894aa26501c1622eed6940c62590727d3ce
Jakub <245762@edu.p.lodz.pl>
2025-03-15 18:26:29 +0100

    Calculator: Finished app

---
+++ app/src/main/java/com/example/calculator/ActivityAbout.kt
@@ -0,0 +1,32 @@
+package com.example.calculator
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActivityAbout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_about)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val textView = findViewById<TextView>(R.id.AboutText)
+
+        val ABOUT_TEXT = """
+Application Calculator v1.0
+Author: Jakub Bak
+Index number: 245762
+Version: 1.0.0
+Release Date: March 2025
+        """
+        textView.text = ABOUT_TEXT
+    }
+}
